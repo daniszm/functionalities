e8eef8942218c5808bb87260e9fab9a1f9826f7a
daniszm <daniszek9933@gmail.com>
2017-03-20 12:07:22 +0100

    Update encryptingPassword.js

--- encryptingPassword.js
+++ encryptingPassword.js
@@ -1,14 +1,8 @@
 // return masked string
 function maskify(cc) {
-
     var lastLetters = cc.slice(-4);
-    console.log("1: " + lastLetters);
-    
     var replaced = cc.replace(/./g, "#").substring(0, cc.length - 4);
-    console.log("2: " + replaced);
-    
     var encryptedWord = replaced + lastLetters;
-    console.log("3: " + encryptedWord);
     
     return encryptedWord;
 }
