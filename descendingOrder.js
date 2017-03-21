5e486e6e4f3e9cbf9d425c45e20df5fc2a4a2da7
daniszm <daniszek9933@gmail.com>
2017-03-21 14:07:03 +0100

    Update descendingOrder.js

--- descendingOrder.js
+++ descendingOrder.js
@@ -1,3 +1,4 @@
+// TASK DESCRIPTION:
 // Your task is to make a function that can take any non-negative integer as a argument and return it with it's digits in descending order.
 // Essentially, rearrange the digits to create the highest possible number.
 
