565fcf0811cf6343500c2defed2c44f2c800a13b
daniszm <daniszek9933@gmail.com>
2017-03-21 14:06:46 +0100

    Update encryptingPassword.js

--- encryptingPassword.js
+++ encryptingPassword.js
@@ -1,4 +1,10 @@
-// return masked string
+// TASK DESCRIPTION:
+// Hide all password and show only last 4 signs.
+
+// Example:
+// input - 1234567890,
+// output - ######7890
+
 function maskify(cc) {
     var lastLetters = cc.slice(-4);
     var replaced = cc.replace(/./g, "#").substring(0, cc.length - 4);
